6639ba293399f4c60bf79b152a51e1c21253ed25
melmnon <166148055+melmnon@users.noreply.github.com>
2024-11-05 16:08:16 +0100

    Add files via upload

---
+++ desarrollopa_3/desarrollopa/Emitter.cpp
@@ -0,0 +1,39 @@
+#include "Emitter.h"
+#include "iostream"
+
+void Emitter::Render()
+{
+	for (int idx = 0; idx < this->particulas.size(); idx++)
+	{
+		this->particulas[idx]->Render();
+	}
+}
+
+void Emitter::Update()
+{
+	{
+		milliseconds currentTime = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
+		//currentTime es el tiempo desde 1970 (tiempo donde windows comenzó a contar) y se lo resta a initialMilliseconds
+		//que es el tiempo en milisegundos hasta ahora, y eso se lo resta al tiempo transcurrido desde al última iteración
+		//de una partícula y se compara con el periodo de emisión
+		if ((currentTime.count() - this->initialMilliseconds.count()) - this->lastUpdatedTime > this->configuracion->GetmiliSeconds())
+		{
+
+			this->particulas.push_back(this->configuracion->GetParticula()->Clone());
+			this->particulas[this->particulas.size() - 1]->SetVelocidad(Vector3D(0, 0, 0));
+			 
+			//se reinicia
+			this->lastUpdatedTime = currentTime.count() - this->initialMilliseconds.count();
+
+			cout << this->lastUpdatedTime << endl;
+		}
+		//vuelves a poner a 0
+		
+	}
+}
+
+
+Solid* Emitter::Clone()
+{
+	return new Emitter(*this);
+}
