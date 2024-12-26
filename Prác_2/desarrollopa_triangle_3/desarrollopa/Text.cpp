4bde8a1a875bb9eb6c65511e3d822b77d9dc1712
MrSae5 <159141787+MrSae5@users.noreply.github.com>
2024-12-26 18:25:10 +0100

    Add files via upload

---
+++ Prác_2/desarrollopa_triangle_3/desarrollopa/Text.cpp
@@ -0,0 +1,18 @@
+#include "Text.h"
+
+void Text::Render()
+{
+    if (text.empty()) return; //Si está vacío, no se hace nada
+
+    glPushMatrix();
+    glColor4f(red, green, blue, alpha);  // Establecer el color
+    glTranslatef(position.GetX(), position.GetY(), position.GetZ());     // Establecer la posición
+    glRasterPos3d(0, 0, 0);         // Posicionar el texto
+
+    // Dibujar cada caracter del texto
+    for (char c : text) {
+        glutBitmapCharacter(font, c);
+    }
+
+    glPopMatrix();
+}
